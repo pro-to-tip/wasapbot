7323982118d7fd3cc0ed7ae7bc40b585efcadcff
pro-to-tip <nenjoi@yahoo.com>
2016-12-06 22:28:55 +0300

    Add files via upload

---
+++ whatsapp/requestCode.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once('src/Registration.php');
+
+$debug = false;
+$username = "";   
+
+if(empty($username)){ 
+echo "Mobile Number can't be Empty"; 
+exit(0);
+}
+if (!preg_match('!^\d+$!', $username))
+{
+  echo "Wrong number. Do NOT use '+' or '00' before your number\n";
+  exit(0);
+}
+$w = new Registration($username, $debug);
+  try {
+    $w->codeRequest("sms");
+	echo "Verification Code Sent via SMS";
+  } catch(Exception $e) {
+    echo $e->getMessage();
+    exit(0);
+  }
+
