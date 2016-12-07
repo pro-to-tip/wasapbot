6b889685a0a9206988fc2c1b65a2bc74b3ef4de8
pro-to-tip <nenjoi@yahoo.com>
2016-12-07 17:45:12 +0300

    Update wasapbot.php

--- wasapbot.php
+++ wasapbot.php
@@ -24,8 +24,8 @@
 
   // bot configuration 
   $nickname = "";                          // bot's nickname
-  $username = "79096693166";                      // bot's number
-  $password = "B8E7584A14E524614DFCB61F2ECFDA5A";    // bot's spassword
+  $username = "79096679572 kIUksreiR9zny7olWXf1C2L9my0=";                      // bot's number
+  $password = "AE37D01C5D531EE826A5AB9B729222C3";    // bot's spassword
   // get the password using registerTool.php
   // --> whatsapp/examples/registerTool.php
 
